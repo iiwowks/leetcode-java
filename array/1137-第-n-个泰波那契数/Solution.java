bbddbe8f816bb1ec06e088f5b851cdb8e0bf2e27
iiwowks <1029871348@qq.com>
2020-08-08 11:47:02 +0800

    8/08

--- array/1137-第-n-个泰波那契数/Solution.java
+++ array/1137-第-n-个泰波那契数/Solution.java
@@ -9,10 +9,10 @@
 class Solution {
     public int tribonacci(int n) {
         if (n == 0) {
-            return 0;
-        }
-        else if (n <= 2) {
-            return 1;
+                return 0;
+            }
+            else if (n <= 2) {
+                return 1;
         }
         return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
