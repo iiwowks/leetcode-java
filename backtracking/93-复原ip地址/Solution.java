59fe98580c86a67b72a4b5e53aba31de41675dd2
iiwowks <1029871348@qq.com>
2020-08-09 12:49:17 +0800

    8/09

---
+++ backtracking/93-复原ip地址/Solution.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=93 lang=java
+ *
+ * [93] 复原IP地址
+ */
+
+// @lc code=start
+class Solution {
+    static final int SEG_COUNT = 4;
+    List<String> ans = new ArrayList<String>();
+    int[] segments = new int[SEG_COUNT];
+
+    public List<String> restoreIpAddresses(String s) {
+        segments = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
+    }
+    public void dfs(String s, int segId, int segStart) {
+        // dfs出口1：找到4段IP地址并且遍历完了字符串，就是一种答案
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuffer ipAddr = new StringBuffer();
+                for (int i = 0; i < SEG_COUNT; ++i) {
+                    ipAddr.append(segments[i]);
+                    if (i != SEG_COUNT - 1) {
+                        ipAddr.append('.');
+                    }
+                }
+                ans.add(ipAddr.toString());
+            }
+            return;
+        }
+        // dfs出口2：如果没有找到4段IP地址就已经遍历完了字符串，提前回溯
+        if (segStart == s.length()) {
+            return;
+        }
+        // dfs入口1：不能有前导零，如果当前数字为零，这一段IP地址只能为0
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
+        // dfs入口2：一般情况，枚举每一种可能性并递归
+        int addr = 0;
+        for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+            addr = addr * 10 + (s.charAt(segEnd) - '0');
+            if (addr > 0 && addr <= 0xFF) {
+                segments[segId] = addr;
+                dfs(s, segId + 1, segEnd + 1);
+            }
+            else {
+                break;
+            }
+        }
+    }
+}
+// @lc code=end
+
