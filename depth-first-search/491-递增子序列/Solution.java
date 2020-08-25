b09cb62c16352212e63f3ec8c67e6c3cb1d0221c
iiwowks <1029871348@qq.com>
2020-08-25 11:42:46 +0800

    8/25

---
+++ depth-first-search/491-递增子序列/Solution.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=491 lang=java
+ *
+ * [491] 递增子序列：找到给定数组的所有递增子序列，长度至少为2
+ */
+// 输入: [4, 6, 7, 7]
+// 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+
+// 方法一：二进制枚举 + 哈希
+// * 长度为n 的序列选择子序列有2^n种情况，2^n种情况可以用在区间[0, 2^(n-1)]内的所有整数的二进制表示。
+// * 解决子序列去重问题：对于序列去重，使用串哈希算法。
+// * 每找到一个合法序列，计算这个序列的哈希值，用一个哈希表记录已有的哈希值。
+
+// @lc code=start
+class Solution {
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+    Set<Integer> set = new HashSet<Integer>();
+    int n;
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        n = nums.length;
+        for (int i = 0; i < (1 << n); ++i) {
+            findSubsequences(i, nums);
+            int hashValue = getHash(263, (int) 1E9 + 7);
+            if (check() && !set.contains(hashValue)) {
+                ans.add(new ArrayList<Integer>(temp));
+                set.add(hashValue);
+            }
+        }
+        return ans;
+    }
+
+    public void findSubsequences(int mask, int[] nums) {
+        temp.clear();
+        for (int i = 0; i < n; ++i) {
+            if ((mask & 1) != 0) {
+                temp.add(nums[i]);
+            }
+            mask >>= 1;
+        }
+    }
+
+    public int getHash(int base, int mod) {
+        int hashValue = 0;
+        for (int x : temp) {
+            hashValue = hashValue * base % mod + (x + 101);
+            hashValue %= mod;
+        }
+        return hashValue;
+    }
+
+    public boolean check() {
+        for (int i = 1; i < temp.size(); ++i) {
+            if (temp.get(i) < temp.get(i - 1)) {
+                return false;
+            }
+        }
+        return temp.size() >= 2;
+    }
+}
+
+// @lc code=end
+
---
+++ depth-first-search/491-递增子序列/Solution2.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=491 lang=java
+ *
+ * [491] 递增子序列：找到给定数组的所有递增子序列，长度至少为2
+ */
+// 输入: [4, 6, 7, 7]
+// 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+
+// @lc code=start
+class Solution {
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        dfs(0, Integer.MIN_VALUE, nums);
+        return ans;
+    }
+
+    public void dfs(int cur, int last, int[] nums) {
+        if (cur == nums.length) {
+            if (temp.size() >= 2) {
+                ans.add(new ArrayList<Integer>(temp));
+            }
+            return;
+        }
+        if (nums[cur] >= last) {
+            temp.add(nums[cur]);
+            dfs(cur + 1, nums[cur], nums);
+            temp.remove(temp.size() - 1);
+        }
+        if (nums[cur] != last) {
+            dfs(cur + 1, last, nums);
+        }
+    }
+}
+
+// @lc code=end
+
