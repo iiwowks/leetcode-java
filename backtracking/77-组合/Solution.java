f117652c1c1033bfd50cc7c410fa7ae83de88b17
iiwowks <1029871348@qq.com>
2020-09-08 13:51:31 +0800

    9/08

--- backtracking/77-组合/Solution.java
+++ backtracking/77-组合/Solution.java
@@ -1,13 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
 /*
  * @lc app=leetcode.cn id=77 lang=java
  *
  * [77] 组合
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
  */
 
 // @lc code=start
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (k <= 0 || n < k) {
+            return res;
+        }
 
+        Deque<Integer> path = new ArrayDeque<>();
+
+        dfs(n, k, 1, path, res);
+        return res;
+    }
+    private void dfs(int n, int k , int begin, Deque<Integer> path, List<List<Integer>> res) {
+        // 终止条件: path.size() == k
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        // 遍历可能的搜索起点
+        for (int i = begin; i <= n; i++) {
+            // 路径变量中添加一个数
+            path.addLast(i);
+            // 下一轮搜索，设置搜索起点 + 1
+            dfs(n, k, i + 1, path, res);
+            // 状态重置
+            path.removeLast();
+        }
     }
 }
 // @lc code=end
---
+++ heap/347-前-k-个高频元素/Solution4.java
@@ -0,0 +1,52 @@
+import java.util.Comparator;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/*
+ * @lc app=leetcode.cn id=347 lang=java
+ *
+ * [347] 前 K 个高频元素
+ *  给定一个非空的整数数组，返回其中出现频率前k高的元素
+ * 
+ * 解题思路： 
+ * 在这里，我们可以利用堆的思想：建立一个小顶堆，然后遍历「出现次数数组」：
+ * 如果堆的元素个数小于 k，就可以直接插入堆中。
+ * 如果堆的元素个数等于 k，则检查堆顶与当前出现次数的大小。如果堆顶更大，说明至少有 k 个数字的出现次数比当前值大，故舍弃当前值；
+ * 否则，就弹出堆顶，并将当前值插入堆中。
+ */
+
+// @lc code=start
+class Solution4 {
+    public int[] topKFrequent(int[] nums, int k) {
+        // key - value
+        Map<Integer, Integer> occurences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurences.put(num, occurences.getOrDefault(num, 0) + 1);
+        }
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] m, int[] n) {
+                return m[1] - n[1];
+            }
+        });
+        //
+        for (Map.Entry<Integer, Integer> entry : occurences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; ++i) {
+            res[i] = queue.poll()[0];
+        }
+        return res;
+    }
+}
+// @lc code=end
+
