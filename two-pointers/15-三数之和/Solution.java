11027483c835781c010af0d0620f0543e11d127f
iiwowks <1029871348@qq.com>
2020-10-04 15:07:22 +0800

    10/4

--- two-pointers/15-三数之和/Solution.java
+++ two-pointers/15-三数之和/Solution.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.List;
-
 /*
  * @lc app=leetcode.cn id=15 lang=java
  *
@@ -10,33 +7,40 @@ import java.util.List;
 // 重点：去重，保持不重复
 // @lc code=start
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {// 总时间复杂度：O(n^2)
-        List<List<Integer>> ans = new ArrayList<>();
-        if (nums == null || nums.length <= 2) return ans;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length <= 2)
+            return res;
+        Arrays.sort(nums);
 
-        Arrays.sort(nums); // O(nlogn)
-
-        for (int i = 0; i < nums.length - 2; i++) { // O(n^2)
-            if (nums[i] > 0) break; // 第一个数大于 0，后面的数都比它大，肯定不成立了
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去掉重复情况
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0)
+                break;
+            // 去重
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             int target = -nums[i];
             int left = i + 1, right = nums.length - 1;
+
+            //
             while (left < right) {
                 if (nums[left] + nums[right] == target) {
-                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
-                    
-                    // 现在要增加 left，减小 right，但是不能重复，比如: [-2, -1, -1, -1, 3, 3, 3], i = 0, left = 1, right = 6, [-2, -1, 3] 的答案加入后，需要排除重复的 -1 和 3
-                    left++; right--; // 首先无论如何先要进行加减操作
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
+                    res.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
                 } else if (nums[left] + nums[right] < target) {
                     left++;
-                } else {  // nums[left] + nums[right] > target
+                } else {
                     right--;
                 }
             }
         }
-        return ans;
+        return res;
     }
+
 }
 // @lc code=end
