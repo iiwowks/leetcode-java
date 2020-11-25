24dcd6aa66a9c800ef45bbea2ad2f04652c0d2f1
iiwowks <1029871348@qq.com>
2020-11-25 21:00:26 +0900

    227 337

---
+++ string/227.basic-calculator-ii/Solution2.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=227 lang=java
+ *
+ * [227] Basic Calculator II
+ */
+
+// @lc code=start
+class Solution2 {
+    public int calculate(String s) {
+        int length = s.length();
+        if (s == null || length == 0) return 0;
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
+    }
+}
+// @lc code=end
+
