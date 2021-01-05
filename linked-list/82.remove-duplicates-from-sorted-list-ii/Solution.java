542e76e6204cdc1ade2bab5fc6d067eafb7cd1d9
iiwowks <1029871348@qq.com>
2021-01-05 23:36:45 +0900

    67 82

---
+++ linked-list/82.remove-duplicates-from-sorted-list-ii/Solution.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=82 lang=java
+ *
+ * [82] Remove Duplicates from Sorted List II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummyHead = new ListNode(0, head);
+        ListNode pre = dummyHead;
+        
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+                pre.next = head.next;     
+            }
+            else {
+                pre = pre.next;    
+            }
+
+            head = head.next;    
+        }  
+        return dummyHead.next;
+    }
+}
+// @lc code=end
+
